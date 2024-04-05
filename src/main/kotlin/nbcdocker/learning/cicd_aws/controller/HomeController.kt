957eab1a898c2159cf26425a0fb5fbef5d1b0c09
harrisbang2 <harrisbang2@gmail.com>
2024-04-05 20:44:01 +0900

    init commit 10

--- src/main/kotlin/nbcdocker/learning/cicd_aws/controller/HomeController.kt
+++ src/main/kotlin/nbcdocker/learning/cicd_aws/controller/HomeController.kt
@@ -5,12 +5,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController {
-
     @GetMapping("/")
     fun home(): String {
         return "home-aws"
     }
-
     @GetMapping("/healthz")
     fun healthz(): String {
         return "healthz-aws"
